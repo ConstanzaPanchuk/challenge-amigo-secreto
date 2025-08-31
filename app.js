a680ada1075bf56430a676b1a5ced4613d889342
ConstanzaPanchuk <constanza.panchuksteinmann@gmail.com>
2025-08-31 19:23:23 -0300

    Implementación de la función agregarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+
+    if (amigo === "") {
+        alert ("Por favor, inserte un nombre");
+    } else {
+        listaAmigos.push(amigo);
+        document.getElementById('amigo').value = "";
+        console.log(listaAmigos);
+    }
+}
