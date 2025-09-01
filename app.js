e9b2c89bf084414ea8889accc9f1727169f270a6
ConstanzaPanchuk <constanza.panchuksteinmann@gmail.com>
2025-08-31 22:09:17 -0300

    implementación de función para mostrar lista

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let listaAmigos = [];
 
+
+//Función que ingresa a la lista los amigos agregados y verifica que no existan valores vacios
 function agregarAmigo() {
     let amigo = document.getElementById('amigo').value;
 
@@ -9,5 +11,23 @@ function agregarAmigo() {
         listaAmigos.push(amigo);
         document.getElementById('amigo').value = "";
         console.log(listaAmigos);
+        mostrarListaAmigos();
     }
 }
+
+//Función que muestra la lista de amigos 
+function mostrarListaAmigos (){
+    //obtengo el elemento lista de html y la vacio
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    //Recorro la lista y creo un elemento li para cada amigo de la lista 
+    for (let i = 0; i < listaAmigos.length; i++) {
+        const amigo = listaAmigos[i];
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
+
+
